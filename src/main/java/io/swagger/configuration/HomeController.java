3852fd81d5ab859eb500350a66504817b54dbe2c
Direccion Ser <katherine.builes@udea.edu.co>
2021-12-19 13:17:16 -0500

    regreso

--- src/main/java/io/swagger/configuration/HomeController.java
+++ src/main/java/io/swagger/configuration/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/value")
+    @RequestMapping(value = "")
     public String index() {
         System.out.println("/swagger-ui/index.html");
         return "redirect:/swagger-ui/";
