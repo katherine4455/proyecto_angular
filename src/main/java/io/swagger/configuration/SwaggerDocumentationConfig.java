739be8a1229d46c26a14bd8b02617ac67ac22cc0
Direccion Ser <katherine.builes@udea.edu.co>
2021-12-19 11:45:09 -0500

    primer

---
+++ src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -0,0 +1,58 @@
+package io.swagger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-14T22:07:29.441Z[GMT]")
+@Configuration
+public class SwaggerDocumentationConfig {
+
+    @Bean
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
+                    .build()
+                .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("OpenAPI FilmStore")
+            .description("This is a sample server FilmStore server. For this sample, you can use the api key `special-key` to test the authorization filters.")
+            .license("Apache-2.0")
+            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.html")
+            .termsOfServiceUrl("")
+            .version("1.0.0")
+            .contact(new Contact("","", ""))
+            .build();
+    }
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("OpenAPI FilmStore")
+                .description("This is a sample server FilmStore server. For this sample, you can use the api key `special-key` to test the authorization filters.")
+                .termsOfService("")
+                .version("1.0.0")
+                .license(new License()
+                    .name("Apache-2.0")
+                    .url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                    .email("")));
+    }
+
+}
---
+++ src/main/java/io/swagger/model/Film.java
@@ -0,0 +1,164 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.model.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * A film for sale in the film store
+ */
+@Schema(description = "A film for sale in the film store")
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-14T22:07:29.441Z[GMT]")
+
+
+public class Film   {
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("category")
+  private Category category = null;
+
+  /**
+   * film status in the store
+   */
+  public enum StatusEnum {
+    AVAILABLED("availabled"),
+    
+    PENDING("pending"),
+    
+    SOLD("sold");
+
+    private String value;
+
+    StatusEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static StatusEnum fromValue(String text) {
+      for (StatusEnum b : StatusEnum.values()) {
+        if (String.valueOf(b.value).equals(text)) {
+          return b;
+        }
+      }
+      return null;
+    }
+  }
+  @JsonProperty("status")
+  private StatusEnum status = null;
+
+  public Film name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Film category(Category category) {
+    this.category = category;
+    return this;
+  }
+
+  /**
+   * Get category
+   * @return category
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public Film status(StatusEnum status) {
+    this.status = status;
+    return this;
+  }
+
+  /**
+   * film status in the store
+   * @return status
+   **/
+  @Schema(description = "film status in the store")
+  
+    public StatusEnum getStatus() {
+    return status;
+  }
+
+  public void setStatus(StatusEnum status) {
+    this.status = status;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Film film = (Film) o;
+    return Objects.equals(this.name, film.name) &&
+        Objects.equals(this.category, film.category) &&
+        Objects.equals(this.status, film.status);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, category, status);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Film {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    category: ").append(toIndentedString(category)).append("\n");
+    sb.append("    status: ").append(toIndentedString(status)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
